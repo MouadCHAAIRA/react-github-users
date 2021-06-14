06117925a0690627aa6c425b56b114ce02493934
MouadCHAAIRA <mouad.chaaira@usmba.ac.ma>
2021-06-14 14:56:45 +0100

    modified

--- src/context/context.js
+++ src/context/context.js
@@ -2,12 +2,27 @@ import React, { useState, useEffect, createContext } from 'react'
 import { db } from '../firebase/config'
 
 const API_URL = 'https://api.github.com/users?per_page=100'
+const SEARCH_URL = 'https://api.github.com/search/users?q='
 
 const GithubUsersContext = createContext()
 
 const GithubUsersProvider = ({ children }) => {
   const [users, SetUsers] = useState([])
   const [loading, setLoading] = useState(false)
+
+  const searchUsers = (query) => {
+    if (query.trim() !== '') {
+      setLoading(true)
+      fetch(`${SEARCH_URL}${query}`)
+        .then((res) => res.json())
+        .then((data) => {
+          SetUsers(data.items)
+          setLoading(false)
+        })
+    } else {
+      getUsers()
+    }
+  }
   const getUsers = () => {
     setLoading(true)
     fetch(API_URL)
@@ -22,7 +37,7 @@ const GithubUsersProvider = ({ children }) => {
     getUsers()
   }, [])
 
-  const values = { users, loading }
+  const values = { users, loading, searchUsers }
   return <GithubUsersContext.Provider value={values} children={children} />
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,51 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useContext, useEffect, useState } from 'react'
 import { GithubUsersContext } from '../context/context'
+import SingleUser from '../components/SingleUser'
 
 // SingleUser component
 // import SingleUser from '../components/SingleUser'
 
 const Home = () => {
-  const { users, loading } = useContext(GithubUsersContext)
+  const { users, loading, searchUsers } = useContext(GithubUsersContext)
+  const [shearch, setSherarch] = useState('')
 
   return (
     <>
-      <h1>Home {loading && 'loading...'} </h1>
-      {!loading &&
-        users.map(({ node_id, login, avatar_url, html_url }) => {
-          return (
-            <div key={node_id}>
-              <h5>{login}</h5>
-              <img width='100' src={avatar_url} alt={login} />
-            </div>
-          )
-        })}
+      <div className='center'>
+        <h1>Home {loading && 'loading...'} </h1>
+      </div>
+      <form
+        style={{ marginBottom: '100px' }}
+        onSubmit={(e) => {
+          e.preventDefault()
+          searchUsers(shearch)
+        }}
+      >
+        <input
+          type='search'
+          className='search-input'
+          placeholder='what are you looking for'
+          value={shearch}
+          onChange={(e) => setSherarch(e.target.value)}
+        />
+      </form>
+
+      <section className='followers'>
+        <div className='center'>
+          <h3>
+            {!loading &&
+              users.length === 0 &&
+              ` No Github Found under the name (${shearch})`}
+          </h3>
+        </div>
+        <div className='container'>
+          {!loading &&
+            users.map((user) => {
+              return <SingleUser user={user} key={user.node_id} />
+            })}
+        </div>
+      </section>
     </>
   )
 }
